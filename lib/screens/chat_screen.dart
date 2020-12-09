c1eba30a9d1ae39aaeeecfd79e84f1e54b013746
Neeraj <palliyali.neeraj@gmail.com>
2020-12-09 19:51:03 +0530

    FireStore for realtime messaging

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/z9qb2vAVkRFRLmNQpyv9/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
       ),
     );
   }
